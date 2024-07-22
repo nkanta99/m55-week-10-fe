4965f601631b0bd7010e1ad1bd9ce420cb7bf33f
isaac nkanta <170721134+nkanta99@users.noreply.github.com>
2024-07-22 13:57:26 +0100

    chore: setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>{import.meta.env.VITE_COOL_TEST}</h1>   
+    </>
+  );
+}
+
+export default App
